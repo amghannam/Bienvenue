8418365d6bc858fbb5cb332d2b0760cfd8464db6
Jake Zarobsky <jzarob@gmail.com>
2016-11-08 08:16:29 -0600

    Added a view model locator to application resources.

---
+++ Bienvenue/Bienvenue/ViewModelLocator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Bienvenue.ViewModel;
+
+namespace Bienvenue
+{
+    public class ViewModelLocator
+    {
+        public CountrySelectionViewModel CountrySelectViewModel
+        {
+            get {
+                return new CountrySelectionViewModel();
+            }
+        }
+    }
+}
